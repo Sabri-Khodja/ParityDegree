9de40b3f23c76d21eed60c7f3ad42e93f2f6b837
Sabri Khodja <sabri.khodja@gmail.com>
2025-05-30 04:14:53 +0200

    implement unit test for case N = 24

--- ParityDegree.Tests/SolutionUnitTests.cs
+++ ParityDegree.Tests/SolutionUnitTests.cs
@@ -14,5 +14,16 @@ namespace SolutionTests
             
             Assert.Equal(0, result);
         }
+
+        [Fact]
+        public void Given_NIs24_When_SolutionIsCalled_Then_Return3()
+        {
+            var solution = new Solution();
+            int N = 24;
+            
+            int result = solution.solution(N);
+            
+            Assert.Equal(3, result);
+        }
     }
 }
