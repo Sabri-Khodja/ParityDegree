280a0cb68bd9e3d37e9022082667e22d391275c9
Sabri Khodja <sabri.khodja@gmail.com>
2025-05-30 05:16:59 +0200

    Add unit test N = 1048576

--- ParityDegree.Tests/SolutionUnitTests.cs
+++ ParityDegree.Tests/SolutionUnitTests.cs
@@ -8,6 +8,8 @@ namespace SolutionTests
         [InlineData(1, 0)]
         [InlineData(24, 3)]
         [InlineData(1000000000, 9)]
+        [InlineData(1048576, 20)]
+
         public void Given_N_When_SolutionIsCalled_Then_ReturnExpectedValue(int n, int expected)
         {
             var solution = new Solution();
