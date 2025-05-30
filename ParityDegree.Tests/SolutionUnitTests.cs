a2cdd01990f0e0635e5cbfc8f4167eeaad28d3e9
Sabri Khodja <sabri.khodja@gmail.com>
2025-05-30 04:06:43 +0200

    Add unit test for Solution with N = 1

--- ParityDegree.Tests/SolutionUnitTests.cs
+++ ParityDegree.Tests/SolutionUnitTests.cs
@@ -1,7 +1,18 @@
-﻿namespace SolutionTests
+﻿using ParityDegree;
+
+namespace SolutionTests
 {
     public class SolutionUnitTests
     {
-
+        [Fact]
+        public void Given_NIs1_When_SolutionIsCalled_Then_Return0()
+        {
+            var solution = new Solution();
+            int N = 1;
+            
+            int result = solution.solution(N);
+            
+            Assert.Equal(0, result);
+        }
     }
 }
